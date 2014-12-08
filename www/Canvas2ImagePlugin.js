9cef2830033a9174aac218af2fe6020f1ee42184
thomasX <thomas@margreiter.info>
2014-12-08 12:54:17 +0100

    Update Canvas2ImagePlugin.js
    added optional parameters fileExtension, quality

--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -9,7 +9,7 @@
 
   module.exports = {
     
-    saveImageDataToLibrary:function(successCallback, failureCallback, canvasId) {
+    saveImageDataToLibrary:function(successCallback, failureCallback, canvasId,fileExtension,quality) {
         // successCallback required
         if (typeof successCallback != "function") {
             console.log("Canvas2ImagePlugin Error: successCallback is not a function");
@@ -17,10 +17,19 @@
         else if (typeof failureCallback != "function") {
             console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
         }
+        else if ((fileExtension) && ((fileExtension.toLowerCase() != '.jpg') && (fileExtension.toLowerCase != '.png'))) {
+            console.log("Canvas2ImagePlugin Error: fileExtension must be '.jpg' or '.png'");
+        }
         else {
             var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
             var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
-            return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
+            var extension='.png';
+            var destQuality=1.0;
+            if (fileExtension) extension=fileExtension.toLowerCase();
+            try{
+              if (quality) destQuality=parseFloat(quality);
+            } catch (e){}
+            return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData,extension,destQuality]);
         }
     }
   };
