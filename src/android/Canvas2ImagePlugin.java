28339289795fae970b5cdec025ede43a3e154301
Elda Zakiryanov <eldar.zakiryanov@gmail.com>
2020-10-22 11:06:15 +0600

    fix: Android permissions

--- src/android/Canvas2ImagePlugin.java
+++ src/android/Canvas2ImagePlugin.java
@@ -2,148 +2,147 @@ package org.devgeeks.Canvas2ImagePlugin;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.Calendar;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 
-/**
- * Canvas2ImagePlugin.java
- *
- * Android implementation of the Canvas2ImagePlugin for iOS.
- * Inspirated by Joseph's "Save HTML5 Canvas Image to Gallery" plugin
- * http://jbkflex.wordpress.com/2013/06/19/save-html5-canvas-image-to-gallery-phonegap-android-plugin/
- *
- * @author Vegard Løkken <vegard@headspin.no>
- */
 public class Canvas2ImagePlugin extends CordovaPlugin {
-	public static final String ACTION = "saveImageDataToLibrary";
+    public static final String ACTION = "saveImageDataToLibrary";
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private JSONArray requestArgs;
+    private CallbackContext callbackContext;
 
-	@Override
-	public boolean execute(String action, JSONArray data,
-			CallbackContext callbackContext) throws JSONException {
+    @Override
+    public boolean execute(String action, JSONArray args,
+                           CallbackContext callbackContext) throws JSONException {
 
-		if (action.equals(ACTION)) {
+        this.callbackContext = callbackContext;
+        this.requestArgs = args;
 
-			String base64 = data.optString(0);
-			String extension = data.optString(1);
-			String quality = data.optString(2);
-			String picfolder= Environment.DIRECTORY_PICTURES;
-			boolean add2Galery=true;
-			if (data.length()>3) picfolder=data.optString(3);
-			if (data.length()>4) add2Galery=Boolean.valueOf(data.optString(4));
-				
-			if (base64.equals("")) // isEmpty() requires API level 9
-				callbackContext.error("Missing base64 string");
-			
-			// Create the bitmap from the base64 string
-			Log.d("Canvas2ImagePlugin", base64);
-			byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
-			Bitmap bmp = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-			if (bmp == null) {
-				callbackContext.error("The image could not be decoded");
-			} else {
-				
-				// Save the image
-				File imageFile = savePhoto(bmp,extension,quality,picfolder);
-				if (imageFile == null)
-					callbackContext.error("Error while saving image");
-				
-				// Update image gallery
-				if (add2Galery) scanPhoto(imageFile);
-				
-				callbackContext.success(imageFile.toString());
-			}
-			
-			return true;
-		} else {
-			return false;
-		}
-	}
+        if (action.equals(ACTION)) {
+            return save(args);
+        } else {
+            return false;
+        }
+    }
 
-	private int getQuality(String strQuality){
-		int result=100;
-		try {
-		    result=Integer.valueOf(strQuality);
-		    if (result> 100) result=100;
-		    if (result < 1) result=1;
-		} catch (Exception e){}		
-		return result;
-	}
-	private File savePhoto(Bitmap bmp,String extension,String strQuality,String picfolder) {
-		int quality=getQuality(strQuality);
-		File retVal = null;
-		
-		try {
-			Calendar c = Calendar.getInstance();
-			String date = "" + c.get(Calendar.DAY_OF_MONTH)
-					+ c.get(Calendar.MONTH)
-					+ c.get(Calendar.YEAR)
-					+ c.get(Calendar.HOUR_OF_DAY)
-					+ c.get(Calendar.MINUTE)
-					+ c.get(Calendar.SECOND);
+    private Boolean save(JSONArray args) {
+        String base64 = args.optString(0);
+        String filename = args.optString(1);
+        String quality = args.optString(2);
+        String picfolder = args.optString(3);
+        Boolean add2Galery = Boolean.valueOf(args.optString(4));
 
-			String deviceVersion = Build.VERSION.RELEASE;
-			Log.i("Canvas2ImagePlugin", "Android version " + deviceVersion);
-			int check = deviceVersion.compareTo("2.3.3");
+        if (base64.isEmpty()) callbackContext.error("Missing base64 string");
 
-			File folder;
-			/*
-			 * File path = Environment.getExternalStoragePublicDirectory(
-			 * Environment.DIRECTORY_PICTURES ); //this throws error in Android
-			 * 2.2
-			 */
-			if (check >= 1) {
-				if (picfolder == Environment.DIRECTORY_PICTURES){
-					folder = Environment
-	//					.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-						.getExternalStoragePublicDirectory(picfolder);
-				}
-				else{
-					folder = new File(picfolder);
-				}
-				if(!folder.exists()) {
-					folder.mkdirs();
-				}
-			} else {
-				folder = Environment.getExternalStorageDirectory();
-			}
-			
-			File imageFile = new File(folder, "c2i_" + date.toString() + extension);
-			CompressFormat compressFormat=(extension.equals(".jpg")) ? Bitmap.CompressFormat.JPEG :Bitmap.CompressFormat.PNG;
-			FileOutputStream out = new FileOutputStream(imageFile);
-			bmp.compress(compressFormat, quality, out);
-			out.flush();
-			out.close();
+        byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+        Bitmap bmp = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        if (bmp == null) {
+            callbackContext.error("The image could not be decoded");
+        } else {
+            if (!hasPermisssion()) {
+                requestPermissions(0);
+            } else {
+                File imageFile = savePhoto(bmp, filename, quality, picfolder);
+                if (imageFile == null)
+                    callbackContext.error("Error while saving image");
 
-			retVal = imageFile;
-		} catch (Exception e) {
-			Log.e("Canvas2ImagePlugin", "An exception occured while saving image: "
-					+ e.toString());
-		}
-		return retVal;
-	}
-	
-	/* Invoke the system's media scanner to add your photo to the Media Provider's database, 
-	 * making it available in the Android Gallery application and to other apps. */
-	private void scanPhoto(File imageFile)
-	{
-		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-	    Uri contentUri = Uri.fromFile(imageFile);
-	    mediaScanIntent.setData(contentUri);	      		  
-	    cordova.getActivity().sendBroadcast(mediaScanIntent);
-	} 
+                if (add2Galery) scanPhoto(imageFile);
+
+                Log.i("Canvas2ImagePlugin", "imageFile.toString(): " + imageFile.toString());
+
+                callbackContext.success(imageFile.toString());
+            }
+        }
+        return true;
+    }
+
+    public boolean hasPermisssion() {
+        for (String p : permissions) {
+            if (!PermissionHelper.hasPermission(this, p)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void requestPermissions(int requestCode) {
+        PermissionHelper.requestPermissions(this, requestCode, permissions);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        PluginResult result;
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
+                this.callbackContext.sendPluginResult(result);
+                return;
+            }
+        }
+
+        switch (requestCode) {
+            case 0:
+                save(this.requestArgs);
+                break;
+        }
+    }
+
+    private int getQuality(String strQuality) {
+        int result = 100;
+        try {
+            result = Integer.valueOf(strQuality);
+            if (result > 100) result = 100;
+            if (result < 1) result = 1;
+        } catch (Exception e) {
+        }
+        return result;
+    }
+
+    private File savePhoto(Bitmap bmp, String filename, String strQuality, String picfolder) {
+        int quality = getQuality(strQuality);
+        File retVal = null;
+
+        try {
+            File folder = new File(Environment.getExternalStorageDirectory(), picfolder);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            File imageFile = new File(folder, filename);
+            CompressFormat compressFormat = Bitmap.CompressFormat.JPEG;
+            FileOutputStream out = new FileOutputStream(imageFile);
+            bmp.compress(compressFormat, quality, out);
+            out.flush();
+            out.close();
+
+            retVal = imageFile;
+        } catch (Exception e) {
+            Log.e("Canvas2ImagePlugin", "An exception occured while saving image: " + e.toString());
+        }
+        return retVal;
+    }
+
+    private void scanPhoto(File imageFile) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(imageFile);
+        mediaScanIntent.setData(contentUri);
+        cordova.getActivity().sendBroadcast(mediaScanIntent);
+    }
 }
--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -1,41 +1,23 @@
-//
-//  Canvas2ImagePlugin.js
-//  Canvas2ImagePlugin PhoneGap/Cordova plugin
-//
-//  Created by Tommy-Carlos Williams on 29/03/12.
-//  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
-//  MIT Licensed
-//
+module.exports = {
 
-  module.exports = {
-    
-    saveImageDataToLibrary:function(successCallback, failureCallback, canvasId,fileExtension,quality,picfolder,add2galery) {
-        // successCallback required
-        if (typeof successCallback != "function") {
-            console.log("Canvas2ImagePlugin Error: successCallback is not a function");
-        }
-        else if (typeof failureCallback != "function") {
-            console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
-        }
-        else if ((fileExtension) && ((fileExtension.toLowerCase() != '.jpg') && (fileExtension.toLowerCase() != '.png'))) {
-            console.log("Canvas2ImagePlugin Error: fileExtension must be '.jpg' or '.png'");
-        }
-        else {
-            var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
-            var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
-            var extension='.png';
-            var destQuality=100;
-            if (fileExtension) extension=fileExtension.toLowerCase();
-            try{
-              if (quality) destQuality=parseFloat(quality);
-            } catch (e){}
-            var params= [imageData,extension,destQuality];
-            if (picfolder) {
-              params=[imageData,extension,destQuality,picfolder];
-              if (add2galery) params.push(add2galery);
-            }
-            return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",params);
-        }
+  saveImageDataToLibrary: function (successCallback, failureCallback, canvasId, fileName, quality, picfolder, add2galery) {
+    // successCallback required
+    if (typeof successCallback != "function") {
+      console.log("Canvas2ImagePlugin Error: successCallback is not a function");
     }
-  };
-  
+    else if (typeof failureCallback != "function") {
+      console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
+    }
+    else {
+      var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
+      var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/, '');
+      var destQuality = 100;
+      try {
+        if (quality) destQuality = parseFloat(quality);
+      } catch (e) { }
+      var params = [imageData, fileName, destQuality, picfolder, add2galery];
+      return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin", "saveImageDataToLibrary", params);
+    }
+  }
+};
+
