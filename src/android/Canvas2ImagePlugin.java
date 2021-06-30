7ad463cbd065f7f3d77285a56094570d98f99fe7
Elda Zakiryanov <eldar.zakiryanov@gmail.com>
2021-06-30 16:10:20 +0300

    fix

--- src/android/Canvas2ImagePlugin.java
+++ src/android/Canvas2ImagePlugin.java
@@ -2,16 +2,22 @@ package org.devgeeks.Canvas2ImagePlugin;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
 import org.json.JSONArray;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 
@@ -19,7 +25,7 @@ public class Canvas2ImagePlugin extends CordovaPlugin {
     public static final String ACTION = "saveImageDataToLibrary";
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext){
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         if (action.equals(ACTION)) {
             return save(args, callbackContext);
         } else {
@@ -32,7 +38,6 @@ public class Canvas2ImagePlugin extends CordovaPlugin {
         String filename = args.optString(1);
         String quality = args.optString(2);
         String picfolder = args.optString(3);
-        Boolean add2Galery = Boolean.valueOf(args.optString(4));
 
         if (base64.isEmpty()) callbackContext.error("Missing base64 string");
 
@@ -41,15 +46,10 @@ public class Canvas2ImagePlugin extends CordovaPlugin {
         if (bmp == null) {
             callbackContext.error("The image could not be decoded");
         } else {
-            File imageFile = savePhoto(bmp, filename, quality, picfolder);
-            if (imageFile == null)
-                callbackContext.error("Error while saving image");
+            Boolean saved = saveImage(bmp, filename, quality, picfolder);
+            if (!saved) callbackContext.error("Error while saving image");
 
-            if (add2Galery) scanPhoto(imageFile);
-
-            Log.i("Canvas2ImagePlugin", "imageFile.toString(): " + imageFile.toString());
-
-            callbackContext.success(imageFile.toString());
+            callbackContext.success();
         }
         return true;
     }
@@ -65,28 +65,45 @@ public class Canvas2ImagePlugin extends CordovaPlugin {
         return result;
     }
 
-    private File savePhoto(Bitmap bmp, String filename, String strQuality, String picfolder) {
+    private Boolean saveImage(Bitmap bitmap, String name, String strQuality, String picfolder) {
         int quality = getQuality(strQuality);
-        File retVal = null;
+        boolean saved = false;
+        OutputStream fos;
 
         try {
-            File folder = new File(cordova.getActivity().getApplicationContext().getExternalFilesDir(null), picfolder);
-            if (!folder.exists()) {
-                folder.mkdirs();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContentResolver resolver = cordova.getContext().getContentResolver();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/" + picfolder);
+                Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                fos = resolver.openOutputStream(imageUri);
+                bitmap.compress(CompressFormat.JPEG, quality, fos);
+                fos.flush();
+                fos.close();
+            } else {
+                String imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + File.separator + picfolder;
+
+                File file = new File(imagesDir);
+
+                if (!file.exists()) {
+                    file.mkdir();
+                }
+
+                File image = new File(imagesDir, name + ".jpg");
+                fos = new FileOutputStream(image);
+                bitmap.compress(CompressFormat.JPEG, quality, fos);
+                fos.flush();
+                fos.close();
+                scanPhoto(image);
             }
-
-            File imageFile = new File(folder, filename);
-            CompressFormat compressFormat = Bitmap.CompressFormat.JPEG;
-            FileOutputStream out = new FileOutputStream(imageFile);
-            bmp.compress(compressFormat, quality, out);
-            out.flush();
-            out.close();
-
-            retVal = imageFile;
+            saved = true;
         } catch (Exception e) {
             Log.e("Canvas2ImagePlugin", "An exception occured while saving image: " + e.toString());
         }
-        return retVal;
+
+        return saved;
     }
 
     private void scanPhoto(File imageFile) {
